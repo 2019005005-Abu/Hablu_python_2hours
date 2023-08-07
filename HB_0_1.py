fe0e14e563a8df10611f7e74520a7e5e3579c865
Abu Al Shahriar Rifat <rifatrifatsharda2686@gmail.com>
2023-08-07 18:32:00 +0600

    Python 2 hours completed

---
+++ HB_0_1.py
@@ -0,0 +1,67 @@
+print("Hello Python")
+print(10+20);
+name='Abu Al Shahriar Rifat';
+print(name);
+st=90.897;
+print(type(name))
+print(type(st));
+myName="I am a  full stack web application developer";
+myName1="Hi "
+
+concatting=myName1+' '+myName;
+print(concatting);
+
+###Boolean Data type
+x=89;
+y=67
+print(x==y);
+
+##String formatting method
+print('This is my  super number');
+num1=879;
+num2=8997;
+print(f"Thiis is {num1} and second number {num2}");
+myName="Abu Al Shahriar Rifat";
+print(f"My Name is {myName}");
+
+##Binary Types 
+
+my=[1,2,3,4,5,6,250];
+b=bytes(my);
+print(my);
+print(b);
+b1=bytearray(my);
+b1[2]=120
+print(b1);
+print(my);
+
+##None Type
+x=None;
+print(x);
+print(type(x));
+
+'''
+when we use none type
+->when we will see a valueless value 
+then we will have to use none type value
+'''
+
+##sequenceType
+'''
+1.List
+2.Tuple
+3.range
+'''
+
+li=['HTML', 'CSS','BOOTSTRAP','TAILWIND','JAVASCRIPT'
+'TYPESCRIPT', 'REACT JS','NODE JS','EXPRESS JS','MONGODB'];
+print(li);
+print(type(li));
+
+tuple1=(
+    'HTML', 'CSS','BOOTSTRAP','TAILWIND','JAVASCRIPT'
+'TYPESCRIPT', 'REACT JS','NODE JS','EXPRESS JS','MONGODB'
+)
+print(tuple1);
+print(type(tuple1));
+
