e5ccb444f791c36ff3705486d06bfbbc3c904e7b
Abu Al Shahriar Rifat <rifatrifatsharda2686@gmail.com>
2023-08-09 17:01:03 +0600

    2.30 hourse Completed

--- HB_1_2.py
+++ HB_1_2.py
@@ -1,1 +1,77 @@
-print("I am a full stack developer");
+a=('Bangladesh')
+print(type(a));
+##Range Type Data
+ran=range(6);
+print(ran)
+print(type(ran));
+for i in ran:
+    print(i);
+
+##Operator
+'''
+1.Arithmetic Operator
+2.Assignment Operator
+3.Comparison Operator
+4.Logical Operator
+5.Identity Operator
+6.Membership Operator
+7.Bitwise Operator
+'''
+##1.Arithmetic Operator
+num1=90;
+num2=190;
+num3=150
+num4=50
+sum=num1+num2;
+sub=num1-num2;
+mul=num1*num2;
+div=num1/num2;
+mod=num1%num2;
+exponential=num1**2;
+floor_division=num3//num4;
+
+print(sum);## ->+
+print(sub);##->-
+print(mul);##->*
+print(div);##->/
+print(mod);##->%
+print(exponential);##-> **
+print(floor_division);##-> //
+
+##2.Assignment Operator
+d=5
+ass1=d+5;
+print(ass1);
+'''
++= 
+-=
+/=
+*=
+'''
+d=10;
+d=+20;
+d=d+20;
+d=10+20;
+print(d);
+e=+10;
+e=20
+print(e);
+##3.Comparison Operator
+'''
+1.==
+2.!=
+3.>
+4.<
+5.>=
+6.<=
+'''
+##4.Logical Operator
+'''
+1.and
+2.or
+3.not
+'''
+##5.Identity Operator
+##6.Membership Operator
+##7.Bitwise Operator
+
