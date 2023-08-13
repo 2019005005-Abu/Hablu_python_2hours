9d8234e99226a7efb7518c312220ea8d0fb63517
Abu Al Shahriar Rifat <rifatrifatsharda2686@gmail.com>
2023-08-14 02:49:32 +0600

    Python 3 houres has completed

---
+++ HB_3.00.py
@@ -0,0 +1,43 @@
+tuple1=(10,20,30,40);
+print(tuple1[0])
+a=10
+b=20
+c=0;
+
+[a,b]=[b,a]
+print("a->",a);
+print("b->",b);
+
+user__name=input("Enter Name:");
+print("User-Name",user__name);
+UserName=input('Enter Your Name');
+print("User-Name",UserName);
+UserPassword=input("Enter Your Password");
+int(UserPassword);
+print("User_Password"+UserPassword);
+
+##Python_Lists
+my_Language=['Javascript','Typescript','React','Redux'];
+print(my_Language[0])
+'''
+1.List is changeable
+2.We will have to use comma (,)
+3.List may be integer,float or boolean type
+'''
+my_Language[0]='Node';
+print(my_Language);
+
+my_Number=[100,200,300,400,500];
+##Updatewe List
+my_Number[2]=1000;
+print(my_Number);
+
+Hablu=["Hab_1","Hab_2","Hab_3"];
+Hablu[2]='Rifat_Hab';
+print(Hablu);
+
+
+
+
+
+
